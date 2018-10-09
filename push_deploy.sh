71804e6c1f95e5788f094e184d50e6b7c2e46aab
Chirag Jain <csjain@ncsu.edu>
2018-10-09 18:29:19 -0400

    changed push

--- push_deploy.sh
+++ push_deploy.sh
@@ -12,16 +12,21 @@ curl https://raw.githubusercontent.com/silinternational/ecs-deploy/master/ecs-de
  sudo tee -a /usr/bin/ecs-deploy
 sudo chmod +x /usr/bin/ecs-deploy
 
-# login AWS ECR
-eval $(aws ecr get-login --region us-east-1)
-
 # or login DockerHub
 echo "$REGISTRY_PASS" | docker login --username "$REGISTRY_USER" --password-stdin
 
+# login AWS ECR
+echo $(aws ecr get-login --no-include-email --region us-east-2)
+
 # build the docker image and push to an image repository
-docker build --tag erchiragjain92/node-test-app .
-docker tag "$IMAGE_NAME" "${IMAGE_NAME}:latest"
-docker push "${IMAGE_NAME}:latest"
+docker build -t node-test-app .
+docker tag node-test-app:latest 812939987703.dkr.ecr.us-east-2.amazonaws.com/node-test-app:latest
+docker push 812939987703.dkr.ecr.us-east-2.amazonaws.com/node-test-app:latest
+
+# build the docker image and push to an image repository
+# docker build --tag erchiragjain92/node-test-app .
+# docker tag "$IMAGE_NAME" "${IMAGE_NAME}:latest"
+# docker push "${IMAGE_NAME}:latest"
 
 # update an AWS ECS service with the new image
-ecs-deploy -c $CLUSTER_NAME -n $SERVICE_NAME -i $IMAGE_REPO_URL:latest
+# ecs-deploy -c $CLUSTER_NAME -n $SERVICE_NAME -i $IMAGE_REPO_URL:latest
