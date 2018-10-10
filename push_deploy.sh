32bc98f3c09f78b1d73e26e697c864a2b2a459dd
Chirag Jain <csjain@ncsu.edu>
2018-10-09 22:32:33 -0400

    try heroku

--- push_deploy.sh
+++ push_deploy.sh
@@ -1,34 +1,21 @@
-# install AWS SDK
-pip install --user awscli
-export PATH=$PATH:$HOME/.local/bin
+# Install Heroku CLI
+curl https://cli-assets.heroku.com/install-ubuntu.sh | sh
 
-# install necessary dependency for ecs-deploy
-add-apt-repository -y ppa:eugenesan/ppa
-apt-get update
-apt-get install jq -y
-
-# install ecs-deploy
-curl https://raw.githubusercontent.com/silinternational/ecs-deploy/master/ecs-deploy | \
- sudo tee -a /usr/bin/ecs-deploy
-sudo chmod +x /usr/bin/ecs-deploy
-
-# or login DockerHub
+# login DockerHub
 echo "$REGISTRY_PASS" | docker login --username "$REGISTRY_USER" --password-stdin
 
-# # login AWS ECR
-# echo $(aws ecr get-login --no-include-email --region us-east-2)
-
-# # build the docker image and push to an image repository
-# docker build -t node-test-app .
-# docker tag node-test-app:latest 812939987703.dkr.ecr.us-east-2.amazonaws.com/node-test-app:latest
-# docker push 812939987703.dkr.ecr.us-east-2.amazonaws.com/node-test-app:latest
-
-# build the docker image and push to an image repository
+# build the docker image and push to DockerHub repository
 docker build --tag erchiragjain92/node-test-app .
 docker tag "$IMAGE_NAME" "${IMAGE_NAME}:latest"
 docker push "${IMAGE_NAME}:latest"
 
-# update an AWS ECS service with the new image
-# ecs-deploy -c $CLUSTER_NAME -n $SERVICE_NAME -i $IMAGE_REPO_URL:latest
+# login to Heroku repository
+echo "$HEROKU_API_KEY" | docker login --username=_ --password-stdin registry.heroku.com
 
-docker run -it --rm -e AWS_ACCESS_KEY_ID="$AWS_ACCESS_KEY_ID" -e AWS_SECRET_ACCESS_KEY="$AWS_SECRET_ACCESS_KEY" -e AWS_DEFAULT_REGION=us-east-2 ecs-deploy --cluster test --service-name test_srv -i hub.docker.com/r/erchiragjain92/node-test-app:latest
+# Push to Heroku repository
+docker tag erchiragjain92/node-test-app registry.heroku.com/node-test-app-cj/web
+docker push registry.heroku.com/node-test-app-cj/web
+
+# Release new image
+heroku login
+heroku container:release web
