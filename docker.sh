b1777af4456b7307b347c4e12cedfe85001bf4c2
Chirag Jain <csjain@ncsu.edu>
2018-11-11 10:46:39 -0500

    Removed Heroku push

--- docker.sh
+++ docker.sh
@@ -5,7 +5,3 @@ echo "$REGISTRY_PASS" | docker login --username "$REGISTRY_USER" --password-stdi
 docker build --tag erchiragjain92/node-test-app .
 docker tag "$IMAGE_NAME" "${IMAGE_NAME}:latest"
 docker push "${IMAGE_NAME}:latest"
-
-# Push to Heroku repository
-docker tag erchiragjain92/node-test-app registry.heroku.com/node-test-app-cj/web
-docker push registry.heroku.com/node-test-app-cj/web
