acb61602fce805cc39b35f058a41efc8b8e39e8b
Chirag Jain <csjain@ncsu.edu>
2018-11-13 13:12:37 -0500

    try variable

--- ansible.sh
+++ ansible.sh
@@ -2,7 +2,7 @@
 echo $ANSIBLE_VAULT_PASS > vault_pass_file
 
 # Run the ansible playbook
-ansible-playbook -i "${ANSIBLE_INVENTORY}" "${ANSIBLE_PLAYBOOK}" --vault-password-file vault_pass_file
+ansible-playbook -i "${ANSIBLE_INVENTORY}" "${ANSIBLE_PLAYBOOK}" --vault-password-file vault_pass_file --extra-vars "build_env=beta" 
 
 # Remove Vault file
 rm -f vault_pass_file
