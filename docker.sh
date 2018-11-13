5e7612f1020d99442126d19614e25ad45e7a4cc9
Chirag Jain <csjain@ncsu.edu>
2018-11-13 16:48:57 -0500

    Removed extra tagging

--- docker.sh
+++ docker.sh
@@ -4,6 +4,5 @@ printf "Building docker image for ${DEPLOY_ENV}\n\n"
 echo "$REGISTRY_PASS" | docker login --username "$REGISTRY_USER" --password-stdin
 
 # build the docker image and push to DockerHub repository
-docker build --tag erchiragjain92/node-test-app .
-docker tag "$IMAGE_NAME" "${IMAGE_NAME}:${DEPLOY_ENV}"
+docker build --force-rm --tag "${IMAGE_NAME}:${DEPLOY_ENV}" .
 docker push "${IMAGE_NAME}:${DEPLOY_ENV}"
